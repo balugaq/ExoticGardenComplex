ee2ede0bc99c81dec467a122b80a37540abaed0c
luckiest <3043199144@qq.com>
2025-05-07 05:13:57 +0800

    修复空手左键空气会报错

--- src/main/java/io/github/thebusybiscuit/exoticgarden/listeners/PlantsListener.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/listeners/PlantsListener.java
@@ -81,8 +81,19 @@ public class PlantsListener implements Listener {
 
         Block b = e.getClickedBlock();
         ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+
+        // 空手不处理
+        if (item == null || item.getType().isAir() || !item.hasItemMeta()) {
+            return;
+        }
+
         final ItemMeta itemMeta = item.getItemMeta();
+        if (itemMeta == null) {
+            return;
+        }
+
         final Optional<String> id = Slimefun.getItemDataService().getItemData(itemMeta);
+
         if (b != null && id.isPresent() && id.get().equals(FluffyItems.WATERING_CAN.getItemId()) && e.getHand() == EquipmentSlot.HAND) {
             waterStructure(b.getLocation(), e, item);
         }
